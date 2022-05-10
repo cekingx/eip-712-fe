432570ad0c8853dab6ba0cbba2f1ac75e3393d6e
Dirga Yasa <cekingx@gmail.com>
2022-05-10 12:57:37 +0800

    base

---
+++ readme.md
@@ -0,0 +1,9 @@
+# How to run
+Maap agak noob jadinya perlu 2 terminal agar hot reload jalan
+```
+npm run watch
+```
+dan
+```
+npm run start
+```
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/app.js',
+  mode: 'development',
+  output: {
+    filename: 'bundle.js',
+    path: `${__dirname}/dist`,
+    publicPath: `${__dirname}/dist`
+  },
+  devServer: {
+    static: {
+      directory: `${__dirname}/dist`,
+    },
+    port: 9000
+  },
+  plugins: [
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'src/*',
+          to: '[name][ext]',
+          globOptions: {
+            ignore: ['**/*.js']
+          }
+        }
+      ]
+    })
+  ]
+}
